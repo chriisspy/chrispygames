b5b319c4b5312067a2b8ef8ffc296ead83adf140
chriisspy <christian@chrispystudios.co>
2026-06-14 23:43:57 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* Chrispy Games Service Worker
    Change VERSION every time you update the site. */
 
-const VERSION = "chrispy-games-v2026-06-11-1209";
+const VERSION = "chrispy-games-v2026-06-11-1210";
 const CACHE_NAME = VERSION;
 
 const CORE_ASSETS = [
