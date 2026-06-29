46d58ef6ea93584d6dca0679a1971b3b0453d51c
chriisspy <christian@chrispystudios.co>
2026-06-29 20:11:04 +1000

    Add multi-spy Spyfall mode

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* Chrispy Games Service Worker
    Change VERSION every time you update the site. */
 
-const VERSION = "chrispy-games-v2026-06-15-1233";
+const VERSION = "chrispy-games-v2026-06-29-multispy";
 const CACHE_NAME = VERSION;
 
 const CORE_ASSETS = [
