8b2f932af3b2fa5eaeab7c13b230b3be6b4216ef
chriisspy <christian@chrispystudios.co>
2026-06-30 20:00:23 +1000

    Polish Hot Seat animation and results UI

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* Chrispy Games Service Worker
    Change VERSION every time you update the site. */
 
-const VERSION = "chrispy-games-v2026-06-30-hotseat";
+const VERSION = "chrispy-games-v2026-06-30-hotseat-ui2";
 const CACHE_NAME = VERSION;
 
 const CORE_ASSETS = [
