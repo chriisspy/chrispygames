b95532fcc6e8ae236e6cf8734c8c473581c7a28f
chriisspy <christian@chrispystudios.co>
2026-07-01 15:30:32 +1000

    Randomize Hot Seat rounds and selection

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* Chrispy Games Service Worker
    Change VERSION every time you update the site. */
 
-const VERSION = "chrispy-games-v2026-06-30-hotseat-question-fix";
+const VERSION = "chrispy-games-v2026-07-01-hotseat-random-general";
 const CACHE_NAME = VERSION;
 
 const CORE_ASSETS = [
