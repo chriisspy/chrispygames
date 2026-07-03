fdcc9a24ecfa2a31bdc1dd3dcad6ff0cdc9165cd
chriisspy <christian@chrispystudios.co>
2026-07-03 21:57:37 +1000

    Retry Pages deployment for centered Hot Seat UI

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* Chrispy Games Service Worker
    Change VERSION every time you update the site. */
 
-const VERSION = "chrispy-games-v2026-07-03-hotseat-centered-4";
+const VERSION = "chrispy-games-v2026-07-03-hotseat-centered-5";
 const CACHE_NAME = VERSION;
 
 const CORE_ASSETS = [
